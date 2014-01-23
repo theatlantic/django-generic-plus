c03167080b571ef928f05a8d7bee54dbf2e36a0e
Frankie Dintino <fdintino@theatlantic.com>
2014-01-23 13:39:09 -0500

    Bump version to v1.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except IOError:
 
 setup(
     name='django-generic-plus',
-    version="1.0.0",
+    version="1.0.1",
     install_requires=[],
     description="Django model field that combines the functionality of "
                 "GenericForeignKey and FileField",
