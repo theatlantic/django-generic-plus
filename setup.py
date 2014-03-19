49774146051a0cea189f146ef06dfb9194cd26b9
Carl Johnson <cjohnson@theatlantic.com>
2014-03-19 17:04:39 -0400

    v1.0.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except IOError:
 
 setup(
     name='django-generic-plus',
-    version="1.0.4",
+    version="1.0.5",
     install_requires=[],
     description="Django model field that combines the functionality of "
                 "GenericForeignKey and FileField",
